83bbd44473c6f854f933a1dab663ebede0df84fd
codewithalimizbani <alimizbani.js@gmail.com>
2023-11-16 15:10:24 +0330

    add assets and layout and fixing clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,27 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Next.js + Clerk Starter",
+  title: "Second DevOverFlow",
+  description: "A place to ask questions and get answers",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -14,9 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
